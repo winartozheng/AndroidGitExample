f7a539418286c566158aba818488bd6e064c2a9e
winartozheng <winarto.xyz@gmail.com>
2016-06-15 17:19:38 +0700

    commit ke 4

---
+++ app/src/main/java/com/example/winter/gitexample/GetExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.winter.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_example);
+    }
+}
